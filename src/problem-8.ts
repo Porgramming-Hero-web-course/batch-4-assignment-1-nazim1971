75ef0331a790c06f64fb75849d8fbcb049a91279
nazim1971 <md.nazimuddinaj@gmail.com>
2024-11-08 15:09:31 +0600

    solved problem -8

---
+++ src/problem-8.ts
@@ -0,0 +1,10 @@
+type ValidateKeys = <T extends object>(obj: T, keys: (keyof T)[]) => boolean;
+
+const validateKeys: ValidateKeys = (obj, keys) => {
+    for (const key of keys) {
+      if (!(key in obj)) {
+        return false;
+      }
+    }
+    return true;
+  };
